d27b60b947abfcf8502244f3115b16d45bf5b753
manas dubey <dubey28.manas@gmail.com>
2023-01-09 14:54:01 +0530

    16

--- 13_Bear_and_BigBrother_simple_math.cpp
+++ 13_Bear_and_BigBrother_simple_math.cpp
@@ -1,5 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+
 #define fast ios_base::sync_with_stdio(false)
 int main(){
   fast;
---
+++ 16temp.cpp
@@ -0,0 +1,3 @@
+#include<bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false)
+using namespace std;
